e6418e1f3b1d3907bba616010f99a1576449dfbe
Alex Ramos <oemaildoalexr@gmail.com>
2022-02-26 00:14:15 -0300

    Git init

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About Us</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quae, incidunt vero aperiam explicabo minus reiciendis cupiditate mollitia temporibus ratione nemo iste aut eveniet similique natus optio architecto labore amet!</p>
+  <a href="/guides">View guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, rem suscipit
+    voluptates quos cum voluptatibus nisi voluptas quaerat laudantium tempora
+    tempore accusamus accusantium nesciunt veniam animi quod quis aperiam
+    dolorum.
+  </p>
+  <a href="/guides">View guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
